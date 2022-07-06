77d47182b8e08e177689866de4042cccaa994d38
stephen <stephn2027@gmail.com>
2022-07-06 13:25:28 +0900

    added dynamic routing to render pages

---
+++ pages/posts/[id].js
@@ -0,0 +1,30 @@
+import Layout from '../../components/layout';
+import { getAllPostIds, getPostData } from '../../lib/posts';
+
+export default function Post({ postData }) {
+    const {title,id,date}=postData
+    return (
+    <Layout>
+      {title}
+      <br />
+      {id}
+      <br />
+      {date}
+    </Layout>
+  );
+}
+
+export async function getStaticProps({ params }) {
+  const postData = getPostData(params.id);
+  return {
+    props: { postData },
+  };
+}
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
