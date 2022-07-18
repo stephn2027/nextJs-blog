fd2b14bacc27f458857a4025815d3309ac639f85
stephen <stephn2027@gmail.com>
2022-07-18 12:02:03 +0900

    added dynamic routes and links

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
+import { getSortedPostsData } from '../lib/posts';
+import Link from 'next/link';
+import Date from '../components/date';
 import utilStyles from '../styles/utils.module.css';
-import { getSortedPostsData } from '../lib/posts'
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -12,22 +14,22 @@ export async function getStaticProps() {
   };
 }
 
-
-
-export default function Home({allPostsData}) {
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
-      
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date}></Date>
+              </small>
             </li>
           ))}
         </ul>
@@ -35,7 +37,3 @@ export default function Home({allPostsData}) {
     </Layout>
   );
 }
-
-      
-
-     
